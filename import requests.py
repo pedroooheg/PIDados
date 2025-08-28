8a90f6dc27e2c50c2677ecb89f6b0805b2f86202
pedroooheg <pedrohenriquegomes2@hotmail.com>
2025-08-28 17:39:51 -0300

    correcao de seguranca

--- import requests.py
+++ import requests.py
@@ -1,6 +1,11 @@
 import requests
 import json
 
+
+caminho_token = r"C:\Users\Pedro\Documents\token.txt"
+with open(caminho_token, "r", encoding="utf-8") as f:
+    token_appcheck = f.read().strip()
+
 url = "https://southamerica-east1-mobile-testes.cloudfunctions.net/disponilidadesParaMedicamentos"
 
 headers = {
@@ -9,7 +14,7 @@ headers = {
     "origin": "https://e-saudesp.prefeitura.sp.gov.br",
     "referer": "https://e-saudesp.prefeitura.sp.gov.br/",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-    "x-firebase-appcheck": "eyJraWQiOiIwMHlhdmciLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxOjQ5MTc5NTU3OTk2NTp3ZWI6MzI5ZjZjZjQ1NjAxMGE4ZDcyOTY1NCIsImF1ZCI6WyJwcm9qZWN0c1wvNDkxNzk1NTc5OTY1IiwicHJvamVjdHNcL21vYmlsZS10ZXN0ZXMiXSwicHJvdmlkZXIiOiJyZWNhcHRjaGFfZW50ZXJwcmlzZSIsImlzcyI6Imh0dHBzOlwvXC9maXJlYmFzZWFwcGNoZWNrLmdvb2dsZWFwaXMuY29tXC80OTE3OTU1Nzk5NjUiLCJleHAiOjE3NTU3NDEzMDUsImlhdCI6MTc1NTczNzcwNSwianRpIjoiVkxRbkFKZ0VFM0VuS0JLQ2ZIVmpFTS13WDE5ZmVUcE9ZTThnLTNWSnVyQSJ9.B1lSbm3Yvz8RUZIaLnXFJG4MIvR4WhNn4C4p4hfoTyvDF2_sXJ-BAsBcheKkJ69dAIMJwAjIfY_qcMzFP8mCl3KVASQxGnxJTGBBpVmiQiSW-VsHJPYhhiz0dgLpvtHANOu2ldVNPsLdWSJOpFM82JHPZmixcbLTkITEEAsrcipl_y37BhzQ1KA7Dazs-E2ztYrKMqfqyLsdlJ2c-HXLpB8duAUzBQazMYFTfZpiUn3cG0QzNIF_p2MZnGx20Q83iXxbwreT1d9-m40Uv_BOVEydN0PrB3ElhOin-8GTssZfEamR32sJPWBQCtmac8zL2PIfAZQJr7TRDJMM1aqEFyidw6OQDHXCZ568odGU555VjRys1sRTz-g4wdT--84RrTULinWrrsuCZneXA_TChunoYRSxeHNB8uqy_AdE3x7D3RbIhhi_TthZMI4meaMWj5xRHa7ERUzOM1HRy9kxrFm1-AckHtnTx2DEVteOyrO4kcoHYWzwyDxbGduLqfZ3"  # copie o token válido do DevTools
+    "x-firebase-appcheck": token_appcheck
 }
 
 payload = {
@@ -35,7 +40,7 @@ if response.status_code == 200:
         data = response.json()  # Converte a resposta em dicionário Python
         with open("resposta_api.json", "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)  # Salva formatado
-        print("✅ Resposta salva em 'resposta_api.json'")
+        print("Resposta salva em 'resposta_api.json'")
     except Exception as e:
         print("Erro ao converter para JSON:", e)
 else:
